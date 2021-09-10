71bcfbafbfcf6be0ef41c8ac5223042071b4756c
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-09-10 15:55:25 -0600

    finsihed part 2

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #connecting to the server
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #creating function that makes a request to the server
+    for line in log_file: #looping through the data in the server
+        line = line.rstrip() #Taking a specific part off of a line of data
+        day = line[0:3] #defining day should be the first index (day of week) and show the descriuption of what happened
+        if day == "Mon": #if statement defineing what need to happen in order for the next line to take place (so if it isn't tuesday/monday it wont print)
+            print(line) #displaying the variable line on the command line
 
 
-sales_reports(log_file)
+sales_reports(log_file) #closing connection to server
